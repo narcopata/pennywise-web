79a4eb34bbd553f16cb931e6c38c51e3e4cd597e
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-03-21 22:48:37 -0300

    chore: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), vanillaExtractPlugin(), TanStackRouterVite()],
+	resolve: {
+		alias: {
+			"~app": "/src/app",
+		},
+	},
+});
