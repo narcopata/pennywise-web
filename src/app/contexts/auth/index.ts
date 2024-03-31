8d4a271cb24e816935df366946b3a5165b218178
Rodrigo Walace <rodrigo.walace.silva@gmail.com>
2024-03-31 17:08:18 -0300

    style: applying formatter

--- src/app/contexts/auth/index.ts
+++ src/app/contexts/auth/index.ts
@@ -2,9 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "./context";
 
 export const useAuthContext = () => {
-	const context = useContext(
-		AuthContext
-	);
+	const context = useContext(AuthContext);
 
 	if (!context) {
 		throw new Error("Uso inválido do contexto.");
